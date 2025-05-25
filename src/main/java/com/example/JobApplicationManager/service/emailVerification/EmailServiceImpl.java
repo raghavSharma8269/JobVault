af8fd23241cfaf3ca99350e21f0466a863810cdd
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-05-25 16:01:08 -0400

    update names

--- src/main/java/com/example/JobApplicationManager/service/emailVerification/EmailServiceImpl.java
+++ src/main/java/com/example/JobApplicationManager/service/emailVerification/EmailServiceImpl.java
@@ -19,7 +19,7 @@ public class EmailServiceImpl{
     public void sendSimpleMessage(
             String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("JobTrackr <noreply@jobtrackr.com>");
+        message.setFrom("JobVault <noreply@jobvault.com>");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
